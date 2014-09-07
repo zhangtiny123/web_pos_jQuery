203be0478d7ac953f4d980363573c95867be200a
zhangtiny123 <tzhang920314@gmail.com>
2014-09-07 20:52:36 +0800

    finished the web_pos with jQuery

--- app/scripts/cart.js
+++ app/scripts/cart.js
@@ -3,19 +3,101 @@
  */
 $(document).ready(function(){
     var cart_items = JSON.parse(localStorage.cart_items);
-    var item_list = printInventory(cart_items);
-    var item_string = '';
+    if (cart_items.length != 0){
+        var item_list = printInventory(cart_items);
+        var item_string = '';
 
-    _.each(item_list, function(item) {
-        item_string =
-            '<tr class="product_item" data-barcode="'+item.barcode+'">\
+        _.each(item_list, function(item) {
+            if(item.promotion_number == 0){
+                item_string =
+                    '<tr class="cart_item" data-barcode="'+item.barcode+'">\
                 <td>'+item.type+'</td>\
                 <td>'+item.name+'</td>\
                 <td>'+item.price+'</td>\
                 <td>'+item.unit+'</td>\
-                <td>'+item.count+'</td>\
-                <td>'+item.total_price+'</td>\
+                <td>\
+                    <div class="btn-group">\
+                    <button class="btn btn-default count_minus">-</button>\
+                    <button class="btn btn-default count_value" disabled="disabled">'+item.count+'</button>\
+                    <button class="btn btn-default count_add">+</button>\
+                    </div>\
+                </td>\
+                <td class="item_price">'+item.total_price+'</td>\
             </tr>';
-        $("#cart_item_body").append(item_string);
-    })
+            }
+            else{
+                item_string =
+                    '<tr class="cart_item" data-barcode="'+item.barcode+'">\
+                <td>'+item.type+'</td>\
+                <td>'+item.name+'</td>\
+                <td>'+item.price+'</td>\
+                <td>'+item.unit+'</td>\
+                <td>\
+                    <div class="btn-group">\
+                    <button class="btn btn-default count_minus">-</button>\
+                    <button class="btn btn-default count_value" disabled="disabled">'+item.count+'</button>\
+                    <button class="btn btn-default count_add">+</button>\
+                    </div>\
+                </td>\
+                <td class="item_price">'+item.total_price+'元(原价：'+item.price*item.count+'元)'+'</td>\
+            </tr>';
+            }
+
+            $("#cart_item_body").append(item_string);
+
+        });
+        var panel_tail =
+            '<div class="my_panel_tail">\
+                <span>总计：<span id="total_payments">0.00</span>元</span>\
+                <p><a id="pay_button" class="btn btn-primary btn-lg" href="pay_page.html">付款</a></p>\
+            </div>';
+        $("#cart_panel").append(panel_tail);
+        $("#cart_count").text(cart_items.length);
+        $("#total_payments").text(calculate_total_payments(item_list));
+    }
+    else {
+        var empty_message =
+            '<div class="my_empty_message">\
+                <p>购物车为空</p>\
+            </div>';
+        $("#main_cart_panel").append(empty_message);
+    }
+
+
+
+    $(".count_minus").on("click", function() {
+        var barcode = $(this).closest(".cart_item").data("barcode");
+        var stored_items = JSON.parse(localStorage.cart_items);
+        console.log("到这里没有问题。");
+        console.log("index:"+stored_items.indexOf(barcode));
+        stored_items.splice(stored_items.indexOf(barcode), 1);
+        console.log("删除元素没有问题。");
+        localStorage.cart_items = JSON.stringify(stored_items);
+
+        refresh(this);
+    });
+
+    $(".count_add").on("click", function() {
+        var barcode = $(this).closest(".cart_item").data("barcode");
+        var stored_items = JSON.parse(localStorage.cart_items);
+        stored_items.push(barcode);
+        localStorage.cart_items = JSON.stringify(stored_items);
+
+        refresh(this);
+    });
+
+
+    function refresh(location_string){
+        var cart_items = JSON.parse(localStorage.cart_items);
+        var bought_item_list = printInventory(cart_items);
+        var clicked_barcode = $(location_string).closest(".cart_item").data("barcode");
+        var clicked_item = _.find(bought_item_list, function(item) {
+            return item.barcode == clicked_barcode;
+        });
+        $(location_string).closest(".cart_item").find(".count_value").text(clicked_item.count);
+        $(location_string).closest(".cart_item").find(".item_price").text(clicked_item.total_price);
+        $("#cart_count").text(cart_items.length);
+        $("#total_payments").text(calculate_total_payments(bought_item_list));
+
+    }
 });
--- app/scripts/pay_page.js
+++ app/scripts/pay_page.js
@@ -1,3 +1,60 @@
 /**
  * Created by tiny on 14-9-5.
  */
+$(document).ready(function() {
+    var cart_items = JSON.parse(localStorage.cart_items);
+    var bought_item_list = printInventory(cart_items);
+    var bought_item = '';
+    var promotion_item = '';
+
+    _.each(bought_item_list, function(item){
+        if (item.promotion_number == 0) {
+            bought_item =
+                '<tr>\
+                    <td>'+item.type+'</td>\
+                    <td>'+item.name+'</td>\
+                    <td>'+item.price+'</td>\
+                    <td>'+item.unit+'</td>\
+                    <td>'+item.count+'</td>\
+                    <td>'+item.total_price+'元</td>\
+                </tr>'
+        }
+        else {
+            bought_item =
+                '<tr>\
+                    <td>'+item.type+'</td>\
+                    <td>'+item.name+'</td>\
+                    <td>'+item.price+'</td>\
+                    <td>'+item.unit+'</td>\
+                    <td>'+item.count+'</td>\
+                    <td>'+item.total_price+'元(原价：'+item.count*item.price+'元)</td>\
+                </tr>';
+
+            promotion_item =
+                '<tr>\
+                    <td>'+item.type+'</td>\
+                    <td>'+item.name+'</td>\
+                    <td>'+item.promotion_number+'</td>\
+                </tr>';
+        }
+
+        $("#bought_list_body").append(bought_item);
+        if (promotion_item.length != 0) {
+            $("#promotion_body").append(promotion_item);
+        }
+
+        bought_item = '';
+        promotion_item = '';
+    });
+
+    $("#pay_panel_body").prepend(current_time());
+    $("#cart_count").text(cart_items.length);
+    $("#total_payments").text(calculate_total_payments(bought_item_list));
+    $("#saved_money").text(calculate_saved_money(bought_item_list));
+
+    $("#total_pay_button").on("click", function() {
+        var stored_items = JSON.parse(localStorage.cart_items);
+        stored_items = [];
+        localStorage.cart_items = JSON.stringify(stored_items);
+    })
+});
